f2b1821b66a90fcfd3a0825d60be912e38ad86e5
kratos2377 <shobityadav23@gmail.com>
2025-02-22 21:31:50 +0530

    Fixing CameraScanner code

--- app/home/qr_scanner.tsx
+++ app/home/qr_scanner.tsx
@@ -1,70 +1,130 @@
-import { View, Text, StyleSheet, SafeAreaView, Pressable, Platform, StatusBar, AppState } from "react-native";
-import  React, {useRef , useEffect} from "react"
-import { CameraView, useCameraPermissions } from "expo-camera";
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, Alert, BackHandler} from 'react-native';
 import { HomeNavProps } from '../utils/HomeParamList';
-import { Overlay } from "../../components/Overlay";
+import { CameraScanner } from "@/components/CameraScanner";
+import {StyleSheet} from 'react-native';
+import { getShadowProps, goToSettings } from '@/helpers';
+import { RESULTS } from 'react-native-permissions';
+import { EPermissionTypes, usePermissions } from '@/hooks/usePermissions';
 
 export default function QRScannerScreen({ navigation, route }: HomeNavProps<'qr_scanner'>) {
 
-  const [permission, requestPermission] = useCameraPermissions();
+  const {askPermissions} = usePermissions(EPermissionTypes.CAMERA);
+  const [cameraShown, setCameraShown] = useState(false);
+  const [qrText, setQrText] = useState('');
 
-  const isPermissionGranted = Boolean(permission?.granted);
+  let items = [
+    {
+      id: 1,
+      title: 'QR code Scanner',
+    },
+  ];
 
-  const qrLock = useRef(false);
-  const appState = useRef(AppState.currentState);
+  function handleBackButtonClick() {
+    if (cameraShown) {
+      setCameraShown(false);
+    }
+    return false;
+  }
 
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === "active"
-      ) {
-        qrLock.current = false;
-      }
-      appState.current = nextAppState;
-    });
-
+    BackHandler.addEventListener('hardwareBackPress', handleBackButtonClick);
     return () => {
-      subscription.remove();
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        handleBackButtonClick,
+      );
     };
   }, []);
 
-  return (
-    <SafeAreaView style={StyleSheet.absoluteFillObject}>
-      {Platform.OS === "android" ? <StatusBar hidden /> : null}
-      <CameraView
-        style={StyleSheet.absoluteFillObject}
-        facing="back"
-        onBarcodeScanned={({ data }) => {
-          if (data && !qrLock.current) {
-            qrLock.current = true;
-            setTimeout(async () => {
-             // await Linking.openURL(data);
-            }, 500);
+  const takePermissions = async () => {
+    askPermissions()
+      .then(response => {
+        //permission given for camera
+        if (
+          response.type === RESULTS.LIMITED ||
+          response.type === RESULTS.GRANTED
+        ) {
+          setCameraShown(true);
+        }
+      })
+      .catch(error => {
+        if ('isError' in error && error.isError) {
+          Alert.alert(
+            error.errorMessage ||
+              'Something went wrong while taking camera permission',
+          );
+        }
+        if ('type' in error) {
+          if (error.type === RESULTS.UNAVAILABLE) {
+            Alert.alert('This feature is not supported on this device');
+          } else if (
+            error.type === RESULTS.BLOCKED ||
+            error.type === RESULTS.DENIED
+          ) {
+            Alert.alert(
+              'Permission Denied',
+              'Please give permission from settings to continue using camera.',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {text: 'Go To Settings', onPress: () => goToSettings()},
+              ],
+            );
           }
-        }}
-      />
-      {/* Disabling this as this does not work in web properly*/}
-      {/* <Overlay /> */}
-    </SafeAreaView>
+        }
+      });
+  };
+
+  const handleReadCode = (value: string) => {
+    console.log(value);
+    setQrText(value);
+    setCameraShown(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      {items.map(eachItem => {
+        return (
+          <TouchableOpacity
+            onPress={takePermissions}
+            activeOpacity={0.5}
+            key={eachItem.id}
+            style={styles.itemContainer}>
+            <Text style={styles.itemText}>{eachItem.title}</Text>
+          </TouchableOpacity>
+        );
+      })}
+      {cameraShown && (
+        <CameraScanner
+          setIsCameraShown={setCameraShown}
+          onReadCode={handleReadCode}
+        />
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "black",
-    justifyContent: "space-around",
-    paddingVertical: 80,
+    paddingHorizontal: 24,
+    backgroundColor: 'white',
   },
-  title: {
-    color: "white",
-    fontSize: 40,
+  itemContainer: {
+    width: '100%',
+    height: 70,
+    backgroundColor: 'white',
+    marginTop: 30,
+    justifyContent: 'center',
+    ...getShadowProps(),
+    paddingLeft: 20,
   },
-  buttonStyle: {
-    color: "#0E7AFE",
-    fontSize: 20,
-    textAlign: "center",
+  itemText: {
+    fontSize: 17,
+    color: 'black',
   },
 });
--- types/camera_types.ts
+++ types/camera_types.ts
@@ -1,3 +1,18 @@
+
+import {RESULTS} from 'react-native-permissions';
+
 export enum EPermissionTypes {
     CAMERA = 'camera',
   }
+
+
+export type TUsePermissionsReturnType = {
+  isError?: boolean;
+  type: (typeof RESULTS)[keyof typeof RESULTS];
+  errorMessage?: string;
+};
+
+export interface ICameraScannerProps {
+  setIsCameraShown: (value: boolean) => void;
+  onReadCode: (value: string) => void;
+}
