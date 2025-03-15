e1e2b0e0df51002dfc80a342fbeb95f5e289289d
kratos2377 <shobityadav23@gmail.com>
2025-03-15 23:06:05 +0530

    Minor fixes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,9 @@
 import "./polyfills";
 import React, { useMemo } from 'react';
-import { Platform } from 'react-native';
 import { useColorScheme } from './hooks/useColorScheme';
 import BaseScreen from './app/base_screen';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { getRandomValues as expoCryptoGetRandomValues } from "expo-crypto";
-import { Buffer } from "buffer";
-import { DEVNET_ENDPOINT } from './rpc/constants';
-import { createDefaultAddressSelector, createDefaultAuthorizationResultCache, createDefaultWalletNotFoundHandler, SolanaMobileWalletAdapter } from '@solana-mobile/wallet-adapter-mobile';
 import { PaperProvider } from 'react-native-paper';
 import { ClusterProvider } from './components/cluster/cluster-data-access';
 import { ConnectionProvider } from './utils/ConnectionProvider';
